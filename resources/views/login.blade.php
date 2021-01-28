016f70cc1cb6a8fd1d5d1e6d4c80c3b81aeea178
mir20000 <mir20000@users.noreply.github.com>
2021-01-28 19:17:35 +0530

    login and session added

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,18 +5,19 @@
     <div class="row">
         <div class="col-sm-3"></div>
         <div class="col-sm-6 my-3 p-3 card border-warning mb-3">
-            <form>
+            <form action="login" method="POST">
                 <h2>Login</h2>
+                @csrf
                 <div class="form-group">
                   <label for="exampleInputEmail1">Email address</label>
-                  <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                  <input name="email" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" required>
                   <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
                 </div>
                 <div class="form-group">
                   <label for="exampleInputPassword1">Password</label>
-                  <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                  <input name="password" type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" required>
                 </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="submit" class="btn btn-primary">Login</button>
               </form>
         </div>
         <div class="col-sm-3"></div>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +15,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/login', function () {
     return view('login');
 });
-Route::get('/', function () {
-    return view('login');
+Route::get('/logout', function () {
+    Session::forget('user');
+    return redirect('login');
 });
+
+Route::post("/login",[UserController::class,'login']);
+Route::get("/",[ProductController::class,'index']);
